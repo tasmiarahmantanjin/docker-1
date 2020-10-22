262995acf5ebfca6ce7bed2330f3030c16711b1a
Tasmia Rahman <tasmiata@gmail.com>
2020-10-22 15:45:55 +0300

    updated solution

---
+++ 01_dockerfiles/ex03/run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# How to build it?
+docker build -t ex03 .
+
+# How to run it?
+docker run -it --rm -p 8080:80 -p 8022:22 -p 8443:443 --privileged ex03
+
+# HOW TO TEST IF EVRYTHING IS WIRKING OR NOT
+
+# GO-TO a web browser:
+# http://192.168.99.100:8080
+# We need to create first a new password, after that we can log in wiht root and use GitLabCE.
+# my usename: root
+# my password: rootroot
+# Then you need to create a repo and then go to that repo and clone it via terminal like:
+# git clone http://192.168.99.100/root/test-repo.git (remember to use your ip in clone)
+# finally, you are able to push stuff in your repo
+# TA-DA you are done!!!
