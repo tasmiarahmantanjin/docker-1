e0691fb95e4e1ce31dd2dbd8e4b2b1d4324df419
Tasmia Rahman <tasmiata@gmail.com>
2020-10-22 15:43:31 +0300

    updated solution

--- 01_dockerfiles/ex01/run.sh
+++ 01_dockerfiles/ex01/run.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
+
 echo "Building your image..."
 docker build -t ex01 .
 # Run in the background & publish to the port
 echo "Build completed. Now it's Running with this ID..."
-docker run -it --name my_teamspeak --rm -p 9987:9987/udp -p 10011:10011 -p 30033:30033 ex01
+docker run -it -d --name my_teamspeak --rm -p 9987:9987/udp -p 10011:10011 -p 30033:30033 ex01
 echo "Server is running."
+
 # Connect with local client to $(docker-machine ip Char)."
-#echo "When finished, run \`docker stop my_teamspeak\`"
+# echo "When finished, run \`docker stop my_teamspeak\`"
+
+# How to test it?
+# After runnig the run.sh, server will run in the background
+# docker logs my_teamspeck ::: TO CHECK TTHE TOKERN NUMBER
+# GO TO TS3 CLINET AND USER CHAR IP AS A SERVER NAME AND TOKEN ID AS PASS
