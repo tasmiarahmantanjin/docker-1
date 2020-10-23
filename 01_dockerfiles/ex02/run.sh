ecbf2245f8dfa0543a231c0986bba3cd8dbcbde2
Tasmia Rahman <tasmiata@gmail.com>
2020-10-23 11:29:10 +0300

    updated scripts

--- 01_dockerfiles/ex02/run.sh
+++ 01_dockerfiles/ex02/run.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
-echo "go inside of the ft_rails folder"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
+printf "${RED}TEST FOR EX02 OF DOCKERFILE\n";
+printf "${GREEN}Go inside of the ft_rails folder\n";
 cd ft-rails/
 
-echo "clone the ruby app"
-#git clone https://github.com/RailsApps/rails-signup-thankyou.git app
+printf "${RED}clone the ruby app\n";
+git clone https://github.com/RailsApps/rails-signup-thankyou.git app
 
-echo "build the ft-rails:on-build... inside of ft_rails folder"
+printf "${GREEN}build the ft-rails:on-build... inside of ft_rails folder\n";
 docker build -t ft-rails:on-build .
 
-echo "Go back to root directory"
+printf "${RED}Go back to root directory\n";
 cd ..
 
-# After you build the ft-rails:on-build run this commands
-echo "Build your image"
+printf "${GREEN}Build the image\n";
 docker build -t ex02 .
 
-echo "Build completed. Now it's Running with this ID..."
+printf "${RED}Build completed. Now it's Running with the ID...\n";
+printf "${RED}Congratulations!!! go check: 192.168.99.100:3000\n";
 docker run -it --rm -p 3000:3000 ex02
-
-echo "Go check the app from browser: 192.168.99.100:3000"
