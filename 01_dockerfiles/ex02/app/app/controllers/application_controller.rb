10b9498b3ad71d6cf2236ec927b3693618cbd1dc
Tasmia Rahman <tasmiata@gmail.com>
2020-10-23 10:03:56 +0300

    remove app

--- 01_dockerfiles/ex02/app/app/controllers/application_controller.rb
+++
@@ -1,7 +0,0 @@
-class ApplicationController < ActionController::Base
-
-  def after_sign_in_path_for(resource)
-    thankyou_path
-  end
-
-end
--- 01_dockerfiles/ex02/app/app/controllers/thank_you_controller.rb
+++
@@ -1,2 +0,0 @@
-class ThankYouController < ApplicationController
-end
